8c1e1a73c2f8918ac36179f709c823815e0c91cb
Michael Meierhoff <michael.meierhoff@gmail.com>
2017-11-26 22:14:52 +0100

    Initial Cookma Project Setup.

---
+++ src/test/kotlin/de/cookma/recipeManagement/CookmaApplicationTests.kt
@@ -0,0 +1,16 @@
+package de.cookma.recipeManagement
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class CookmaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
