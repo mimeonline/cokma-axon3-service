5b05ab2e6c7466df7b2df35dd9a23d5646e506d5
Michael Meierhoff <michael.meierhoff@gmail.com>
2018-01-28 11:45:17 +0100

    Deactivate Spring Security for the moment

--- src/main/kotlin/de/cookma/authservice/infrastructure/config/AuthConfiguration.kt
+++ src/main/kotlin/de/cookma/authservice/infrastructure/config/AuthConfiguration.kt
@@ -1,12 +1,9 @@
 package de.cookma.authservice.infrastructure.config
 
-import org.springframework.context.annotation.Bean
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-
 class AuthConfiguration {
 
-    @Bean
-    fun bCryptPasswordEncoder(): BCryptPasswordEncoder {
-        return BCryptPasswordEncoder()
-    }
+//    @Bean
+//    fun bCryptPasswordEncoder(): BCryptPasswordEncoder {
+//        return BCryptPasswordEncoder()
+//    }
 }
