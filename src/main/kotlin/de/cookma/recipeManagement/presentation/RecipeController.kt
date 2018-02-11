488d4a405560bae46720237cc60b838c42c0b9ed
Michael Meierhoff <michael.meierhoff@gmail.com>
2018-02-11 18:56:21 +0100

    Clean up code

--- src/main/kotlin/de/cookma/recipeManagement/presentation/RecipeController.kt
+++ src/main/kotlin/de/cookma/recipeManagement/presentation/RecipeController.kt
@@ -1,26 +1,11 @@
 package de.cookma.recipeManagement.presentation
 
 import de.cookma.recipeManagement.application.RecipeApplicationService
-import de.cookma.recipeManagement.infrastructure.store.RecipeImageStore
 import de.cookma.recipeManagement.application.dtoModel.RecipeDto
-import de.cookma.recipeManagement.application.viewModel.RecipeViewModel
-import de.cookma.recipeManagement.domain.model.CreateRecipeCommand
-import de.cookma.recipeManagement.domain.model.RecipeDeletedEvent
-import de.cookma.recipeManagement.domain.model.UpdateRecipeCommand
-import org.axonframework.commandhandling.model.AggregateLifecycle
-import org.axonframework.commandhandling.model.AggregateLifecycle.markDeleted
-import org.axonframework.eventsourcing.EventSourcingHandler
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.core.io.Resource
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
-import org.springframework.web.multipart.MultipartFile
 import java.util.concurrent.CompletableFuture
-import org.springframework.http.HttpHeaders
-import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.ResponseBody
-import org.springframework.web.bind.annotation.GetMapping
 
 
 @RestController
@@ -32,35 +17,28 @@ class RecipeController {
     lateinit var recipeApplicationService: RecipeApplicationService
 
 
-
     @GetMapping
     fun getAllRecipe(): CompletableFuture<List<*>>? {
         return recipeApplicationService.findAllRecipe()
     }
 
     @GetMapping("/{id}")
-    fun getRecipe(@PathVariable("id") id: String): CompletableFuture<RecipeViewModel> {
-        println(id)
-        return recipeApplicationService.findRecipeById(id)
-    }
+    fun getRecipe(@PathVariable("id") id: String) = recipeApplicationService.findRecipeById(id)
+
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    fun createRecipe(@RequestBody recipe: RecipeDto): CompletableFuture<CreateRecipeCommand> {
-        return recipeApplicationService.createRecipe(recipe)
-    }
+    fun createRecipe(@RequestBody recipe: RecipeDto) = recipeApplicationService.createRecipe(recipe)
+
 
     @PutMapping("/{id}")
     fun updateRecipe(
             @RequestBody recipe: RecipeDto,
-            @PathVariable("id") id: String): CompletableFuture<UpdateRecipeCommand> {
-        return recipeApplicationService.updateRecipe(id, recipe)
-    }
+            @PathVariable("id") id: String) = recipeApplicationService.updateRecipe(id, recipe)
+
 
     @DeleteMapping("/{id}")
-    fun deleteRecipe(@PathVariable("id") id: String) {
-        recipeApplicationService.deleteRecipe(id)
-    }
+    fun deleteRecipe(@PathVariable("id") id: String) = recipeApplicationService.deleteRecipe(id)
 
 
 }
